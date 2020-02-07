a986fd7288637fb37f5fddae35256e0bd2157e2d
Kazufumi Hara <you@example.com>
2020-02-07 14:44:59 +0900

    修正0207

--- sample.js
+++ sample.js
@@ -7,18 +7,22 @@ const { Client } = require('pg');
         password: 'postgres',
         database: 'postgres', // ここは、自分か作成したDB名に変更する
         host: '172.17.0.2',
+        // host: 'localhost',
         port: 5432
     });
 
     client.connect();
 
     const query = {
-        text: 'INSERT INTO cars (name, maker) VALUES ($1, $2)',
-        values: ['プリウス', 'TOYOTA']
+        // text: 'INSERT INTO cars (name, maker) VALUES ($1, $2)',
+        // values: ['プリウス', 'TOYOTA']
+        text: 'SELECT * FROM cars'
     };
 
     const resObj = await client.query(query);
-    console.log('insert成功。TBLを確認してください。');
+    console.log(resObj)
+    // console.log('insert成功。TBLを確認してください。');
+    console.log('DBアクセス成功。');
     await client.end();
 })();
 
